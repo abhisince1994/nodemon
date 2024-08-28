454ddae24d59fb1bdd3e2fb330e2005da481f711
abhisince1994 <abhijeetsapkal2@gmail.com>
2024-08-28 22:41:48 +0530

    router used for clen up code and seperate filter are used for file admin.js and shop.js also page not found error 404 added

--- app.js
+++ app.js
@@ -2,26 +2,25 @@ const express = require ('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop')
+
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/add-product',(req,res,next) => {
-    res.send(`
-        <form action="/product" method="POST">
-        <input type="text" name="title"><br>
-        <input type="text" name="size"><br>
-        <button type="submit">Add Product</button></form>
-        `);
-});
-app.post('/product',(req,res,next) => {
-    console.log(req.body);
-    res.redirect('/');
-})
+//
 // app.use((req,res,next) => {
 //     console.log('In the midleware!');
 //     next(); //Allows the request to continue to the next middleware in line
 // });
+
+
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+
+//adding 404 error if page not found
 app.use((req,res,next) => {
-    //console.log('In another middleware!');
-    res.send('<h1>Hello From Express !</h1>');
+    res.status(404).send('<h1> Page Not Found</h1>');
 });
+
 app.listen(3000);
